a4975cbceb37a3cc972cb6a72a80b5d868fff1f2
wzsayiie <wzsayiie@qq.com>
2020-06-26 11:13:27 +0800

    adjust client test program.

--- cmdclient/main.go
+++ cmdclient/main.go
@@ -1,9 +1,27 @@
+//cmdclient
+
 package main
 
 import (
 	"fmt"
 )
 
+func input() string {
+	var cmd string = ""
+
+	fmt.Printf("input >> ")
+	fmt.Scanf("%s", &cmd)
+
+	return cmd
+}
+
+func run(cmd string) {
+	fmt.Printf("receive: %s\n", cmd)
+}
+
 func main() {
-	fmt.Printf("client start\n")
+	for {
+		var cmd string = input()
+		run(cmd)
+	}
 }
