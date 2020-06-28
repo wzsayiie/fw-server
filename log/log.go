237641ee80e4c4c9aa3871b7133e60cd72b61810
wzsayiie <wzsayiie@qq.com>
2020-06-28 23:09:37 +0800

    display time when printing log.

--- log/log.go
+++ log/log.go
@@ -4,22 +4,35 @@ import (
 	"fmt"
 	"path/filepath"
 	"runtime"
+	"time"
 )
 
 func print(tag string, format string, args []interface{}) {
 
-	var msg string = fmt.Sprintf(format, args...)
+	//time.
+	//
+	//format specifiers:
+	//|
+	//| mm:dd HH:MM:SECOND    yy zone
+	//|	01/02 03:04:05.000 PM 06 0700
+	//|       15           AM
+	//
+	var now string = time.Now().Format("06-01-02 15:04:05.000")
 
-	//Caller(0): print();
-	//Caller(1): I() or E();
-	//Caller(2): the user function.
-	_, path, line, okay := runtime.Caller(2)
+	//caller.
+	//| Caller(0): print();
+	//| Caller(1): I() or E();
+	//| Caller(2): the user function.
+	var _, path, line, okay = runtime.Caller(2)
+
+	//message.
+	var msg string = fmt.Sprintf(format, args...)
 
 	if okay {
 		_, file := filepath.Split(path)
-		fmt.Printf("%s|%s(%04d)|%s\n", tag, file, line, msg)
+		fmt.Printf("%s|%s|%s(%04d)|%s\n", now, tag, file, line, msg)
 	} else {
-		fmt.Printf("%s|?|%s\n", tag, msg)
+		fmt.Printf("%s|%s|?|%s\n", now, tag, msg)
 	}
 }
 
